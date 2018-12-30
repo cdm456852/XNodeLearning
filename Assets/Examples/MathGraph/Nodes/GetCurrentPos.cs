88130a7648fa8a5aea17d787a3e966d47523c15a
cdm456852 <576216884@qq.com>
2018-12-30 12:10:56 +0800

    Update

---
+++ Assets/Examples/MathGraph/Nodes/GetCurrentPos.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace XNode.Examples.MathNodes
+{
+    public class GetCurrentPos:XNode.Node
+    {
+        [Input] public Vector3 CurrentPos;
+        [Output] public float CurrentX;
+        [Output] public float CurrentY;
+        [Output] public float CurrentZ;
+
+        public override object GetValue(NodePort port)
+        {
+            string fieldName= port.fieldName;
+            if(fieldName== "CurrentX")
+            {
+                return CurrentPos.x;
+            }
+            else if(fieldName== "CurrentY")
+            {
+                return CurrentPos.y;
+            }
+            else if(fieldName == "CurrentZ")
+            {
+                return CurrentPos.z;
+            }
+            return default(float);
+        }
+
+    }
+}
